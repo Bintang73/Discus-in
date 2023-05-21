8eda58ba68ec5b2aed474df3fce98c21576b2480
usamah abdul matin <eexvuu@gmail.com>
2023-05-21 17:26:35 +0700

    add pages folder & files

---
+++ lib/pages/sign_in.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:stalkin/theme.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      appBar: AppBar(title: Text('sign In')),
+      body: Column(
+        children: [Text('tes')],
+      ),
+    );
+  }
+}
